8f41cdb12ff0bc07f807032e29cc52d6f7e0d942
Aaron <43335377+ru19wu06@users.noreply.github.com>
2021-08-08 22:19:30 +0800

    新增功能

--- Contract_example/send_SER20.sol
+++ Contract_example/send_SER20.sol
@@ -21,7 +21,7 @@ contract MyContract {
         require(CountFrequency[msg.sender]<5,"Hey! You take too much!");
         
         
-        IERC20 FDOGE = IERC20(address(0x4742DC121d121f4Ae2A5fD23DB6031b32916F4a2));
+        IERC20 FDOGE = IERC20(address(0x361D8552d7Db423eC0cb097a5911fE453d205374));
         FDOGE.transfer(msg.sender, 50000000000000000000000);
         
         emit transfer(msg.sender, CountFrequency[msg.sender]);
@@ -32,7 +32,7 @@ contract MyContract {
     
     function sendFDOGE(address _to, uint256 _amount) external {
         require(msg.sender == contract_sender,"You are not the contract sender Bro!!");
-        IERC20 FDOGE = IERC20(address(0x4742DC121d121f4Ae2A5fD23DB6031b32916F4a2));
+        IERC20 FDOGE = IERC20(address(0x361D8552d7Db423eC0cb097a5911fE453d205374));
         
         
         FDOGE.transfer(_to, _amount);
