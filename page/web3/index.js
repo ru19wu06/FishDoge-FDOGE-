b2051a0678848e8e8856b9bff8fa883bb2c57628
ru19wu06 <43335377+ru19wu06@users.noreply.github.com>
2022-01-20 17:51:16 +0800

    add mint

--- page/web3/index.js
+++ page/web3/index.js
@@ -7,6 +7,7 @@ let coinbase;
 async function RenewAddress(){
 
   coinbase = await web3.eth.getCoinbase();
+  console.log(coinbase);
 
   let ETHbalance = await web3.eth.getBalance(coinbase);
   ETHbalance = web3.utils.fromWei(ETHbalance, 'ether');
@@ -15,5 +16,30 @@ async function RenewAddress(){
   add = add +"....";
   $("#login_address").text(ETHbalance.substring(0,6)+" Eth | "+add);
      
+  setNFT_ABI();
 }
+
+
+//FDOGE_NFT_ABI
+
+var FDOGE_Contract;
+var FDOGE_agiContract;
+
+async function setNFT_ABI(){
+  FDOGE_Contract= "0x361d8552d7db423ec0cb097a5911fe453d205374";
+    
+  FDOGE_agiContract = new web3.eth.Contract(FDOGE_NFT_ABI,FDOGE_Contract);
+}
+
+
+async function MintFdoge(){
+  await FDOGE_agiContract.methods.mint().send({from: coinbase, value:web3.utils.toWei('0.01', 'ether'), gas: 350000 })
+  .then(function(receipt){
+
+    alert("交易成功，你已經買到FDOGE了!");
+  })
+
+}
+
+
 RenewAddress();
